33925ae2f4263a333e7a4bc4e23908faa2ca0b3a
Daiki Hatada <hatadaiki@gmail.com>
2020-06-16 16:41:32 +0900

    Initial Commit

---
+++ SampleForLearningSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleForLearningSwift
+//
+//  Created by Daiki Hatada on 2020/06/16.
+//  Copyright © 2020 Daiki Hatada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
